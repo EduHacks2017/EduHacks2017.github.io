af12b71891aff8a007c4d3a4c7aa7d5c40d2fb30
Snipower <thereceiverofjunkmail@gmail.com>
2017-09-19 00:28:32 -0500

    Hasty fixes

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 let currTab;
 let started=false;
-let inputStr=`def myfunction() some text \n  some other text`;
+let js=`var firstName, lastName, price, discount, fullPrice;\nfirstName = "John";\nlastName = "Doe";\nprice = 19.90;\ndiscount = 0.10;\nfullPrice = price * 100 / discount;`;
+let python=`parents, babies = (1, 1)\nwhile babies < 100:\n print 'This generation has {0} babies'.format(babies)\n parents, babies = (babies, parents + babies)`;
+let cpp=`#include <iostream>\nint main()\n{\nstd::cout << "Hello World!";\nstd::cout << std::endl;\nreturn 0;\n}`;
 let counter =  0;
-
-function switchSelected(obj){
+let inputStr="";
+function switchSelected(obj,id){
   let tabs=document.getElementsByClassName("languageTab");
   for(let i=0;i<tabs.length;i++){
     tabs[i].style.backgroundColor="#f7f7f7";
   }
   currTab=obj;
   currTab.style.backgroundColor="#d3d3d3";
+
+  if(id==0){
+    inputStr=js;
+  }
+
+  if(id==1){
+    inputStr=cpp;
+  }
+
+  if(id==2){
+    inputStr=python;
+  }
+  
 }
 
 function createSpans(str,area){
   let arr=str.split("");
+  area.innerHTML="";
   for(let i=0;i<arr.length;i++){
     let curr=arr[i];
     if(curr=="\n") area.innerHTML+="<div id=i"+i+">"+curr+"</div>";
@@ -22,8 +38,10 @@ function createSpans(str,area){
   }
 }
 
-function start(tab){
-  switchSelected(tab);
+function start(tab,id){
+  switchSelected(tab,id);
+  let text=document.getElementById("text");
+  createSpans(inputStr,text);
   if(started) return;
   started=true;
 
@@ -40,10 +58,6 @@ function start(tab){
     tab.style.width="200px";
     tab.style.fontSize="1.5em";
   }
-
-  let text=document.getElementById("text");
-
-  createSpans(inputStr,text);
 
   let main=document.getElementById("main");
   main.style.opacity=1;
